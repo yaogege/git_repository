5bdcbe4fe7fda3d0794619ef0a96d3b4f3094d9b
yaogege <767320509@qq.com>
2020-04-29 10:55:26 +0800

    新建分支及新建ppy1文件

---
+++ ppy/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# encoding: utf-8
+'''
+@author: ygg
+@time: 2020/4/29 10:48
+'''
+import time
+
---
+++ ppy/ppy1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time:2019/5/27 11:45
+
+import datetime
+
+
+
+if __name__ == "__main__":
+
+    print(datetime.datetime(2020,4,25))
